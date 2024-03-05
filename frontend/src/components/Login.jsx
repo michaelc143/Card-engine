354f62a1dedb222df220dbe11d1e1a020fa61f34
Michael Corbishley <mcorbishley@wisc.edu>
2024-03-05 16:33:17 -0600

    fixing formatting and comments

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,8 +6,8 @@ function Login({ onLogin, onRegister }) {
     const [isRegistering, setIsRegistering] = useState(false);
   
     const handleLogin = () => {
-      // Perform your login logic here (e.g., API call, authentication)
-      // For simplicity, let's just check if username is not empty
+      // Check to see if username is not empty
+      // Sends username and loggedIn to parent component
       if (username) {
         onLogin(username, true);
       } else {
@@ -16,11 +16,11 @@ function Login({ onLogin, onRegister }) {
     };
 
     const handleToggleMode = () => {
+      // Swaps button from register to login and visa versa
       setIsRegistering(!isRegistering);
     };
 
     const handleRegister = () => {
-      // Perform your registration logic here
       // For now just doing the same as logging in
       if (username) {
         onRegister(username, true); //passes the username and login back to parent component
@@ -39,13 +39,25 @@ function Login({ onLogin, onRegister }) {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        {isRegistering ? (
-          <button className='register-btn' onClick={handleRegister}>Register</button>
-        ) : (
-          <button className='login-btn' onClick={handleLogin}>Login</button>
-        )}
+        {
+          isRegistering 
+          ? 
+          (
+            <button className='register-btn' onClick={handleRegister}>Register</button>
+          ) 
+          :
+          (
+            <button className='login-btn' onClick={handleLogin}>Login</button>
+          )
+        }
         <button className='toggle-btn' onClick={handleToggleMode}>
-          {isRegistering ? 'Already have a login? Login Here' : 'New player? Register Here'}
+          {
+          isRegistering 
+          ? 
+          'Already have a login? Login Here'
+          :
+          'New player? Register Here'
+          }
         </button>
       </div>
     );
