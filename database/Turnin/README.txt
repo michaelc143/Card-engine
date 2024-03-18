873a5295aa6f6f98d26d64cfc1acc232a3b455b7
Tomas Valdes <valdes@cs506-team-25.cs.wisc.edu>
2024-03-18 16:01:10 -0500

    removed examples from Devtech

--- database/Turnin/README.txt
+++
@@ -1,2 +0,0 @@
-Aidan Koenig
-Tomas Valdes
--- database/Turnin/Who.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `WhatAndWho`.`Who` (
-  `Who_pk` INT NOT NULL auto_increment,
-  `Who_jj` VARCHAR(45) NULL,
-  `Who_kk` VARCHAR(45) NULL,
-  `Who_What_pk` INT NULL,
-  PRIMARY KEY (`Who_pk`));
