05ffe50760f79ab8ba0c6769e4e528d7a554d0ec
Tomas Valdes <tvaldes60@gmail.com>
2024-04-07 19:52:26 -0500

    Added endpoints to select seat and get list of open games

--- backend/src/main/java/CS506Team25/Card_Engine/CardEngineController.java
+++ backend/src/main/java/CS506Team25/Card_Engine/CardEngineController.java
@@ -7,6 +7,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.StringWriter;
 import java.io.PrintWriter;
@@ -70,10 +72,11 @@ public class CardEngineController {
     }
 
     @PostMapping("/create-game")
-    public String createGame() {
+    public String createGame(@RequestParam String gameName) {
         try (Connection connection = DriverManager.getConnection(url, databaseUsername, password);
-             PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO euchre_game VALUES (DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT)")) {
+             PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO euchre_game VALUES (DEFAULT, ?, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT)")) {
 
+            insertStatement.setString(1, gameName);
             int rowsInserted = insertStatement.executeUpdate();
             if (rowsInserted > 0) {
                 PreparedStatement selectStatement = connection.prepareStatement("SELECT LAST_INSERT_ID()");
@@ -86,6 +89,54 @@ public class CardEngineController {
         } catch (SQLException e) {
             e.printStackTrace();
             return "Error occurred connecting to server.";
+        }
+    }
+
+    @PostMapping("/select-seat")
+    public String assignSeat(@RequestParam int gameID, int playerID, int seatNumber) {
+        String playerSeat = "player" + seatNumber + "_id";
+        try (Connection connection = DriverManager.getConnection(url, databaseUsername, password);
+            PreparedStatement insertStatement = connection.prepareStatement("UPDATE euchre_game SET " + playerSeat + " = ? WHERE game_id = ?")) {
+
+            insertStatement.setInt(1, playerID);
+            insertStatement.setInt(2, gameID);
+            int rowsInserted = insertStatement.executeUpdate();
+            if (rowsInserted > 0) {
+                return "Successfully assigned to seat " + seatNumber;
+            } else {
+                return "Could not assign seat";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "Error occurred connecting to server.";
+        }
+    }
+
+    @GetMapping("/open-games")
+    public ObjectNode getOpenGames() {
+        try (Connection connection = DriverManager.getConnection(url, databaseUsername, password);
+            PreparedStatement statement = connection.prepareStatement("SELECT game_id, game_name, player1_id, player2_id, player3_id, player4_id FROM euchre_game WHERE game_status = 'waiting_for_players'")) {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectNode json = objectMapper.createObjectNode();
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                ObjectNode game = objectMapper.createObjectNode();
+
+                int players = 0;
+                for (int x = 3; x < 7; x++){
+                    if (resultSet.getInt(x) != 0){
+                        players++;
+                    }
+                }
+                game.put("game_id",resultSet.getInt(1));
+                game.put("number_players", players);
+                json.set(resultSet.getString(2), game);
+            }
+            return json;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
