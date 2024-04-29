946a39f5b809d99936add6ad4fda9cd74be14a23
Michael Corbishley <mcorbishley@wisc.edu>
2024-04-29 08:53:22 -0500

    removing extra cors config from engine startup

--- backend/src/main/java/CS506Team25/Card_Engine/CardEngineApplication.java
+++ backend/src/main/java/CS506Team25/Card_Engine/CardEngineApplication.java
@@ -25,17 +25,5 @@ public class CardEngineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CardEngineApplication.class, args);
 	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedOrigins("*")
-					.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
-			}
-		};
-	}
 
 }
