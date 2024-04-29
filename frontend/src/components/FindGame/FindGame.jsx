87d0800a498d6a166f28e62c1eceabd6c1d94117
Michael Corbishley <mcorbishley@wisc.edu>
2024-04-28 20:10:14 -0500

    removing ID from game list

--- frontend/src/components/FindGame/FindGame.jsx
+++ frontend/src/components/FindGame/FindGame.jsx
@@ -51,7 +51,7 @@ function FindGame({ closeModal, openSelectSeatModal }) {
 					{Object.entries(openGames).map(([gameName, gameData]) => (
 							<div key={gameName}>
 								<p style={{fontWeight: 'bold', width: '33%', display: 'inline-block', marginLeft: '0.5rem'}}>
-								{gameName} : ID {gameData.game_id}
+								{gameName}
 								</p>
 								<p style={{fontWeight: 'bold', width: '33%', display: 'inline-block'}}>
 								{gameData.number_players}/4
